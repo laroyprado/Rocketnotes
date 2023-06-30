520751afbc1b5500fa633da79116ed6df60c78b7
Laroy Prado <laroyprado@gmail.com>
2023-06-30 16:33:50 -0300

    Finalização da página Home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,6 +1,11 @@
 import { Container, Brand, Menu, Search, Content, NewNote } from "./styles";
-
+import { FiPlus, FiSearch } from "react-icons/fi";
 import { Header } from "../../components/Header";
+import { Input } from "../../components/Input";
+import { Section } from "../../components/Section";
+
+import { ButtonText } from "../../components/ButtonText";
+import { Note } from "../../components/Note";
 
 export const Home = () => {
   return (
@@ -11,11 +16,44 @@ export const Home = () => {
 
       <Header />
 
-      <Menu></Menu>
-      <Search></Search>
-      <Content></Content>
+      <Menu>
+        <li>
+          <ButtonText title="Todos" isActive />
+        </li>
+        <li>
+          <ButtonText title="React" />
+        </li>
+        <li>
+          <ButtonText title="Nodejs" />
+        </li>
+      </Menu>
+      <Search>
+        <Input placeholder="Pesquisar pelo título" icon={FiSearch} />
+      </Search>
+      <Content>
+        <Section title="Minhas notas">
+          <Note
+            data={{
+              title: "React",
+              tags: [
+                {
+                  id: "1",
+                  name: "react",
+                },
+                {
+                  id: "2",
+                  name: "Rocketseat",
+                },
+              ],
+            }}
+          />
+        </Section>
+      </Content>
 
-      <NewNote></NewNote>
+      <NewNote>
+        <FiPlus />
+        Criar Nota
+      </NewNote>
     </Container>
   );
 };
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -32,17 +32,35 @@ export const Brand = styled.div`
 `;
 export const Menu = styled.ul`
   grid-area: menu;
-  background: green;
+  background-color: ${({ theme }) => theme.COLORS.BACKGROUND_900};
+  padding-top: 64px;
+  text-align: center;
+
+  > li {
+    margin-bottom: 24px;
+  }
 `;
 export const Search = styled.div`
   grid-area: search;
-  background: violet;
+  padding: 64px 64px 0;
+  overflow-y: auto;
 `;
+
 export const Content = styled.div`
   grid-area: content;
-  background: blue;
+  padding: 0 64px;
 `;
+
 export const NewNote = styled.button`
   grid-area: newnote;
-  background: yellow;
+
+  background-color: ${({ theme }) => theme.COLORS.ORANGE};
+  border: none;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  svg {
+    margin-right: 8px;
+  }
 `;
