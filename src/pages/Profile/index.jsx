8e5b86e9425aeb0a18f167b0543714e4f1a9a1b2
Laroy Prado <laroyprado@gmail.com>
2023-06-30 18:23:41 -0300

    Página de Profile Construida

---
+++ src/pages/Profile/index.jsx
@@ -0,0 +1,32 @@
+import { Container, Form, Avatar } from "./styles";
+import { FiArrowLeft, FiUser, FiMail, FiLock, FiCamera } from "react-icons/fi";
+import { Input } from "../../components/Input";
+import { Button } from "../../components/Button";
+
+export const Profile = () => {
+  return (
+    <Container>
+      <header>
+        <a href="/">
+          <FiArrowLeft />
+        </a>
+      </header>
+      <Form>
+        <Avatar>
+          <img src="https://github.com/laroyprado.png" alt="Foto do Usuário" />
+
+          <label htmlFor="avatar">
+            <FiCamera />
+            <input id="avatar" type="file" />
+          </label>
+        </Avatar>
+
+        <Input placeholder="Nome" type="text" icon={FiUser} />
+        <Input placeholder="Email" type="text" icon={FiMail} />
+        <Input placeholder="Senha Atual" type="password" icon={FiLock} />
+        <Input placeholder="Nova Senha" type="password" icon={FiLock} />
+        <Button title="Salvar" />
+      </Form>
+    </Container>
+  );
+};
