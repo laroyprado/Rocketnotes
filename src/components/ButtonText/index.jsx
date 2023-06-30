78c7f2dccf5c92c6d43437308a317898d91673f4
Laroy Prado <laroyprado@gmail.com>
2023-06-30 16:33:29 -0300

    Atualiazação do componente Button e seu estilo

--- src/components/ButtonText/index.jsx
+++ src/components/ButtonText/index.jsx
@@ -1,8 +1,8 @@
 import { Container } from "./styles";
 
-export const ButtonText = ({ title, ...rest }) => {
+export const ButtonText = ({ title, isActive = false, ...rest }) => {
   return (
-    <Container type="button" {...rest}>
+    <Container type="button" {...rest} isActive={isActive}>
       {title}
     </Container>
   );
