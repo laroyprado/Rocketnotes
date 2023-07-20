97cd83769566d49c0d67539f52c5c3c785143dfe
Laroy Prado <laroyprado@gmail.com>
2023-07-20 11:31:51 -0300

    Funcionalidade de Login Adicionada

--- src/pages/Signin/index.jsx
+++ src/pages/Signin/index.jsx
@@ -3,17 +3,38 @@ import { Input } from "../../components/Input";
 import { Button } from "../../components/Button";
 import { FiLogIn, FiMail, FiLock } from "react-icons/fi";
 import { Link } from "react-router-dom";
+import { useAuth } from "../../hooks/auth";
+import { useState } from "react";
 
 export const SignIn = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const { signIn } = useAuth();
+
+  function handleSignIn() {
+    signIn({ email, password });
+  }
+
   return (
     <Container>
       <Form>
         <h1>Rocket Notes</h1>
         <p>Aplicação para salvar e gerenciar seus links úteis.</p>
         <h2>Faça seu login</h2>
-        <Input placeholder="E-mail" type="text" icon={FiMail} />
-        <Input placeholder="Senha" type="password" icon={FiLock} />
-        <Button title="Entrar" />
+        <Input
+          placeholder="E-mail"
+          onChange={(e) => setEmail(e.target.value)}
+          type="text"
+          icon={FiMail}
+        />
+        <Input
+          placeholder="Senha"
+          onChange={(e) => setPassword(e.target.value)}
+          type="password"
+          icon={FiLock}
+        />
+        <Button title="Entrar" onClick={handleSignIn} />
 
         <Link to="/register">Criar conta</Link>
       </Form>
