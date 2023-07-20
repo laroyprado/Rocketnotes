1fd9d3be26b563395956394ac1e40c981a49b371
Laroy Prado <laroyprado@gmail.com>
2023-07-20 14:31:11 -0300

    LocalStorage implementado para salvar usuário

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useState, useEffect } from "react";
 import { api } from "../services/api";
 
 export const AuthContext = createContext({});
@@ -12,6 +12,9 @@ function AuthProvider({ children }) {
       const { user, token } = response.data;
       console.log(user, token);
 
+      localStorage.setItem("@rocketnotes:user", JSON.stringify(user));
+      localStorage.setItem("@rocketnotes:token", token);
+
       api.defaults.headers.authorization = `Bearer ${token}`;
       setData({ user, token });
     } catch (error) {
@@ -23,8 +26,24 @@ function AuthProvider({ children }) {
     }
   }
 
+  useEffect(() => {
+    const token = localStorage.getItem("@rocketnotes:token");
+    const user = localStorage.getItem("@rocketnotes:user");
+
+    if (token && user) {
+      api.defaults.headers.authorization = `Bearer ${token}`;
+
+      setData({
+        token,
+        user: JSON.parse(user),
+      });
+    }
+  }, []);
+
   return (
-    <AuthContext.Provider value={{ signIn,user:data.user }}>{children}</AuthContext.Provider>
+    <AuthContext.Provider value={{ signIn, user: data.user }}>
+      {children}
+    </AuthContext.Provider>
   );
 }
 
